b34884428a0db79412cedace19b5e113b776fc3e
Finnbar Keating <finnjkeating@gmail.com>
2023-02-25 21:26:46 +0000

    Rewrote benchmarks. Didn't fix it...

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -11,7 +11,8 @@ import Hedgehog (Gen)
 import Hedgehog.Gen (sample, just)
 import Control.DeepSeq
 import Data.IORef
-import Control.Monad (replicateM_, void)
+import Control.Monad (replicateM_, void, forM)
+import System.IO (Handle, hFlush, stderr, stdout, hPutStr)
 
 import FRP.Yampa
 import Transform
@@ -23,70 +24,70 @@ import Optimise
 
 type TestPair = (CFSF (V Double) (V Double), SF Double Double)
 
--- guarantee there's five elements
-type TestSet = (TestPair, TestPair, TestPair, TestPair, TestPair)
-
-generateNetworks :: Gen (CFSF (V Double) (V Double), SF Double Double) -> IO TestSet
+generateNetworks :: Gen (CFSF (V Double) (V Double), SF Double Double) -> IO TestPair
 generateNetworks gen = do
-    !a <- makeOne
-    !b <- makeOne
-    !c <- makeOne
-    !d <- makeOne
-    !e <- makeOne
-    return (a,b,c,d,e)
-    where
-        makeOne :: IO TestPair
-        makeOne = do
-            (!cfsf, !sf) <- sample gen
-            let !cfsf' = transform cfsf
-            return (cfsf', sf)
+    (!cfsf, !sf) <- sample gen
+    let !cfsf' = optimiseCFSF $ transform cfsf
+    return (cfsf', sf)
 
-benchThisGenerator :: String -> Gen (CFSF (V Double) (V Double), SF Double Double) -> ([Val (V Double)], [Double]) -> Benchmark
-benchThisGenerator = undefined
--- benchThisGenerator nam gen (ins, ins') = env (generateNetworks gen) $
---     \ ~((cfsf1,sf1), (cfsf2,sf2), (cfsf3,sf3), (cfsf4,sf4), (cfsf5, sf5)) -> bgroup nam [
---         bgroup "net-1" [
---             bench "sfrp" $ nfIO (map simplify <$> runCompCFSF cfsf1 ins),
---             bench "sf" $ nf (embed sf1) (deltaEncode 1 ins')
---         ],
---         bgroup "net-2" [
---             bench "sfrp" $ nfIO (map simplify <$> runCompCFSF cfsf2 ins),
---             bench "sf" $ nf (embed sf2) (deltaEncode 1 ins')
---         ],
---         bgroup "net-3" [
---             bench "sfrp" $ nfIO (map simplify <$> runCompCFSF cfsf3 ins),
---             bench "sf" $ nf (embed sf3) (deltaEncode 1 ins')
---         ],
---         bgroup "net-4" [
---             bench "sfrp" $ nfIO (map simplify <$> runCompCFSF cfsf4 ins),
---             bench "sf" $ nf (embed sf4) (deltaEncode 1 ins')
---         ],
---         bgroup "net-5" [
---             bench "sfrp" $ nfIO (map simplify <$> runCompCFSF cfsf5 ins),
---             bench "sf" $ nf (embed sf5) (deltaEncode 1 ins')
---         ]
---     ]
+benchThisGenerator :: String -> Gen (CFSF (V Double) (V Double), SF Double Double) -> ([Val (V Double)], [Double]) -> IO Benchmark
+benchThisGenerator nam gen (ins, ins') = do
+    (!ccfsf1,!cfsf1,!sf1) <- generateNetworks gen
+    cPSL $ "Generated benchmarks for " ++ nam
+    return $ bgroup nam [
+            bench "cfsf" $ whnfAppIO (benchCFSF cfsf1) ins,
+            bench "sf" $ whnfAppIO (benchSF sf1) ins'
+        ]
 
 generateProgram :: GenParam -> Gen (CFSF (V Double) (V Double), SF Double Double)
 generateProgram gp = just $ genProg ProxV ProxV gp
 
-allGens :: ([Val (V Double)], [Double]) -> [Benchmark]
-allGens inputs = concat $ flip map [25,50,100,150,200,250,300] $
-    \size -> [
-            benchThisGenerator "no-loop" (generateProgram (GP size (Just []))) inputs,
-            benchThisGenerator "one-loop" (generateProgram (GP size (Just [1]))) inputs,
-            benchThisGenerator "deep-loops" (generateProgram (GP size (Just $ sizeToDeep size))) inputs,
-            benchThisGenerator "shallow-loops" (generateProgram (GP size (Just $ sizeToShallow size))) inputs,
-            benchThisGenerator "branching-loops" (generateProgram (GP size (Just $ sizeToBranching size))) inputs,
-            benchThisGenerator "any-loops" (generateProgram (GP size Nothing)) inputs
-        ]
+cPSL :: String -> IO ()
+cPSL s = putStrLn s >> hFlush stdout
+
+cP :: Show a => a -> IO ()
+cP = cPSL . show
+
+main :: IO ()
+main =
+    do
+        cPSL "Running"
+        !inputs <- sample $ genDoubles 100000 --genInputSamples 100000
+        let sizes = [100]--[25,50,100,150,200,250,300]
+        cPSL "Generating noLoop"
+        noLoop <- forM sizes $ \size -> benchThisGenerator (show size) (generateProgram (GP size (Just []))) inputs
+        cPSL "Generating deepLoop"
+        deepLoop <- forM sizes $ \size -> benchThisGenerator (show size) (generateProgram (GP size (Just $ sizeToDeep size))) inputs
+        cPSL "Generating shallowLoop"
+        shallowLoop <- forM sizes $ \size -> benchThisGenerator (show size) (generateProgram (GP size (Just $ sizeToShallow size))) inputs
+        cPSL "Generating mixedLoop"
+        mixedLoop <- forM sizes $ \size -> benchThisGenerator (show size) (generateProgram (GP size (Just $ sizeToBranching size))) inputs
+        cPSL "Done generating"
+        let !benches = [bgroup "no-loop" noLoop, bgroup "deep-loop" deepLoop, bgroup "shallow-loop" shallowLoop, bgroup "mixed-loop" mixedLoop]
+        defaultMainWith defaultConfig benches -- NOTE: will likely need to change default config
     where
+        fraction :: Num a => a
+        fraction = 5
         sizeToDeep :: Int -> [Int]
-        sizeToDeep n = replicate (n `div` 10) 1
+        sizeToDeep n = replicate (n `div` fraction) 1
         sizeToShallow :: Int -> [Int]
-        sizeToShallow n = [n `div` 10]
+        sizeToShallow n = [n `div` fraction]
         sizeToBranching :: Int -> [Int]
-        sizeToBranching n = replicate (floor $ logBase 2.0 (fromIntegral n / 10)) 2
+        sizeToBranching n = replicate (floor $ logBase 2.0 (fromIntegral n / fraction)) 2
+
+
+    -- -- Let's construct some examples just to make sure.
+    -- (!cfsf, !sf) <- sample $ generateProgram (GP 50 (Just [2,2,2])) -- makeMassiveNestedLoop 100
+    -- let !cfsf' = optimiseCFSF $ transform cfsf
+    -- !ccfsf <- compile cfsf'
+    -- print cfsf'
+    -- defaultMainWith defaultConfig [
+    --         bench "cfsf" $ whnfIO $ benchCFSF cfsf' ins,
+    --         bench "sf" $ whnfIO $ benchSF sf ins'
+    --     ]
+
+instance NFData a => NFData (Val ('V a)) where
+    rnf (One a) = rnf a
 
 benchCFSF :: CFSF (V Double) (V Double) -> [Val (V Double)] -> IO ()
 benchCFSF cfsf ins = do
@@ -107,27 +108,11 @@ benchSF sf ins = do
     replicateM_ 100000 $ do
         (i : inps) <- readIORef inputRef
         writeIORef inputRef inps
-        void $ react handle (1, Just i)
+        b <- react handle (1, Just i)
+        forceM b
 
 forceM :: (Monad m, NFData a) => a -> m ()
 forceM val =
   case rnf val of
     () -> pure ()
 
-main :: IO ()
-main = do
-    (!ins, !ins') <- sample $ genDoubles 100000 --genInputSamples 100000
-    -- defaultMainWith defaultConfig (allGens (ins, ins')) -- NOTE: will likely need to change default config
-
-    -- Let's construct some examples just to make sure.
-    (!cfsf, !sf) <- sample $ generateProgram (GP 50 Nothing) -- makeMassiveNestedLoop 100
-    let !cfsf' = optimiseCFSF $ transform cfsf
-    !ccfsf <- compile cfsf'
-    print cfsf'
-    defaultMainWith defaultConfig [
-            bench "cfsf" $ nfIO (benchCFSF cfsf' ins),
-            bench "sf" $ nfIO (benchSF sf ins')
-        ]
-
-instance NFData a => NFData (Val ('V a)) where
-    rnf (One a) = rnf a
