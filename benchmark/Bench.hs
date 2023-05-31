0005e3ee84ce81ff15a989935d0edc8814d1606a
Finnbar Keating <finnjkeating@gmail.com>
2023-05-31 19:52:22 +0100

    More testing

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -4,7 +4,7 @@ import BenchRandom
 import BenchTHTests
 
 import Criterion.Main
-import Criterion.Types (Config(..))
+import Criterion.Types (Config(..), Verbosity(..))
 import Statistics.Types (cl99)
 
 -- RUN WITH
@@ -15,4 +15,4 @@ main = do
     th <- benchTHTests
     rd <- benchRandom
     let benches = [th, rd]
-    defaultMainWith (defaultConfig {csvFile = Just "tests.csv", confInterval = cl99, timeLimit = 20}) benches
+    defaultMainWith (defaultConfig {csvFile = Just "tests.csv", confInterval = cl99, timeLimit = 20, verbosity = Verbose}) benches
